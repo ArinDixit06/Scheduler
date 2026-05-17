1149ec53b6f89d694b61d5df93877f1b656d65b3
ArinDixit06 <id2013663@gmail.com>
2026-05-17 19:54:12 +0530

    adding all files

--- apps/flutter/lib/main.dart
+++ apps/flutter/lib/main.dart
@@ -1,121 +1,155 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'constants/theme.dart';
+import 'providers/auth_provider.dart';
+import 'providers/task_provider.dart';
+import 'providers/calendar_provider.dart';
+import 'providers/habit_provider.dart';
+import 'providers/focus_provider.dart';
+import 'providers/copilot_provider.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/today/today_screen.dart';
+import 'screens/tasks/task_list_screen.dart';
+import 'screens/calendar/calendar_screen.dart';
+import 'screens/focus/focus_screen.dart';
+import 'screens/ai/copilot_screen.dart';
+import 'screens/settings/settings_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ApexApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ApexApp extends StatelessWidget {
+  const ApexApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
+        ChangeNotifierProvider(create: (_) => CalendarProvider()),
+        ChangeNotifierProvider(create: (_) => HabitProvider()),
+        ChangeNotifierProvider(create: (_) => FocusProvider()),
+        ChangeNotifierProvider(create: (_) => CopilotProvider()),
+      ],
+      child: Consumer<AuthProvider>(
+        builder: (context, auth, _) {
+          return MaterialApp(
+            title: 'APEX',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            home: auth.isLoggedIn
+                ? const MainTabNavigator()
+                : LoginScreen(onLoginSuccess: () {}),
+          );
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainTabNavigator extends StatefulWidget {
+  const MainTabNavigator({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainTabNavigator> createState() => _MainTabNavigatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainTabNavigatorState extends State<MainTabNavigator> {
+  int _currentIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _onTabSelect(int index) {
+    setState(() => _currentIndex = index);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final bottomPadding = MediaQuery.of(context).padding.bottom;
+
+    final List<Widget> screens = [
+      TodayScreen(onTabChange: _onTabSelect),
+      const TaskListScreen(),
+      const FocusScreen(),
+      const CalendarScreen(),
+      SettingsScreen(onLogout: () {
+        _onTabSelect(0);
+      }),
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: AppTheme.pureWhite,
+      body: Stack(
+        children: [
+          // Render selected screen contents
+          IndexedStack(
+            index: _currentIndex,
+            children: screens,
+          ),
+
+          // Custom Subtractive Bottom Navigation Bar
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: AppTheme.pureWhite,
+                border: const Border(
+                  top: BorderSide(color: AppTheme.cloudGray, width: 1.0),
+                ),
+              ),
+              padding: EdgeInsets.only(
+                top: AppTheme.space8,
+                bottom: bottomPadding > 0 ? bottomPadding : AppTheme.space8,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildTabItem(0, 'TODAY', Icons.dashboard_outlined),
+                  _buildTabItem(1, 'TASKS', Icons.checklist_outlined),
+                  _buildTabItem(2, 'FOCUS', Icons.timer_outlined),
+                  _buildTabItem(3, 'AGENDA', Icons.calendar_month_outlined),
+                  _buildTabItem(4, 'CONFIG', Icons.settings_outlined),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    );
+  }
+
+  Widget _buildTabItem(int index, String label, IconData icon) {
+    final isSelected = _currentIndex == index;
+
+    return GestureDetector(
+      onTap: () => _onTabSelect(index),
+      behavior: HitTestBehavior.opaque,
+      child: SizedBox(
+        height: AppTheme.space48,
+        width: 60,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            Icon(
+              icon,
+              color: isSelected ? AppTheme.electricBlue : AppTheme.pewter,
+              size: 20,
+            ),
+            const SizedBox(height: 4),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              label,
+              style: AppTheme.captionText.copyWith(
+                fontSize: 9.0,
+                color: isSelected ? AppTheme.electricBlue : AppTheme.pewter,
+                fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+                letterSpacing: 0.5,
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
